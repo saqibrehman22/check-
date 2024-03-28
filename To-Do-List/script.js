2abd3943a1bc447ce16752aeed8d685638fe661f
Saqib Rehman <135380886+saqibrehman22@users.noreply.github.com>
2024-03-29 03:21:07 +0500

    Add files via upload

---
+++ To-Do-List/script.js
@@ -0,0 +1,27 @@
+
+const inputbox= document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function AddTask(){
+    if(inputbox.value ===''){
+        alert("You Must Write SomeThing");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML= inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value ="";
+}
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("check");
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }s
+},false);
